bad72fca04ba1bf1828abab2f5ba16f5aca017b2
PecanPie <hidra0321@naver.com>
2023-01-06 20:12:37 +0900

    Default branch moved main to develop

---
+++ Collageuner/Collageuner/ModelOneDeleteIt.swift
@@ -0,0 +1,8 @@
+//
+//  ModelOneDeleteIt.swift
+//  Collageuner
+//
+//  Created by KYUBO A. SHIM on 2023/01/06.
+//
+
+import Foundation
---
+++ Collageuner/Collageuner/ViewOneDeleteIt.swift
@@ -0,0 +1,8 @@
+//
+//  ViewOneDeleteIt.swift
+//  Collageuner
+//
+//  Created by KYUBO A. SHIM on 2023/01/06.
+//
+
+import Foundation
