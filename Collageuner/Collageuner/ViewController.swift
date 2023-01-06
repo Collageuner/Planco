375742af869f9868c6a7bc8867967944d004899c
PecanPie <hidra0321@naver.com>
2023-01-06 18:16:50 +0900

    :tada: Added temporary Launch Screen and Packages
    Setted for Code base coding and some Packages

---
+++ Collageuner/Collageuner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collageuner
+//
+//  Created by KYUBO A. SHIM on 2023/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
