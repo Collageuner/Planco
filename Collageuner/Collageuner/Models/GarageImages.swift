f1c32e6aac9ce224b57bad23ea992f5101ea1274
PecanPie <hidra0321@naver.com>
2023-01-29 14:10:43 +0900

    :sparkles: Modified GardenImageModels using RealmSwift

--- Collageuner/Collageuner/Models/GarageImages.swift
+++ Collageuner/Collageuner/Models/GarageImages.swift
@@ -8,28 +8,28 @@
 import UIKit
 
 import RealmSwift
-//
-//class GarageLists {
-//    var imageList: [GarageImage]
-//    var counts: Int
-//    var todayDate: Date
-//    
-//    init(imageList: [GarageImage], counts: Int, todayDate: Date) {
-//        self.imageList = imageList
-//        self.counts = counts
-//        self.todayDate = todayDate
-//    }
-//}
-//
-//class GarageImage: Object {
-//    @Persisted(primaryKey: true) var id: ObjectId
-//    @Persisted var garageThumbnailImageURL: String
-//    // 이미지는 disk 에 저장을 하라. Data type 으로 직접 Realm DB 에 올리지 말고
-//    // + 뭐 이런말도 있다. -> Realm Results 에는 Swift 의 .reduce 를 쓰지마라. 매우 느려진다. 대신 Realm Filter() 를 사용해라.
-//    
-//    convenience init(garageThumbnailImages: String) {
-//        self.init()
-//        
-//        self.garageThumbnailImageURL = garageThumbnailImages
-//    }
-//}
+
+class GarageLists {
+    var imageList: [GarageImage]
+    var counts: Int
+    var todayDate: Date
+
+    init(imageList: [GarageImage], counts: Int, todayDate: Date) {
+        self.imageList = imageList
+        self.counts = counts
+        self.todayDate = todayDate
+    }
+}
+
+class GarageImage: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var garageThumbnailImageURL: String
+    // 이미지는 disk 에 저장을 하라. Data type 으로 직접 Realm DB 에 올리지 말고
+    // + 뭐 이런말도 있다. -> Realm Results 에는 Swift 의 .reduce 를 쓰지마라. 매우 느려진다. 대신 Realm Filter() 를 사용해라.
+
+    convenience init(garageThumbnailImages: String) {
+        self.init()
+
+        self.garageThumbnailImageURL = garageThumbnailImages
+    }
+}
