0c8310095fcc5fb3ce02fa3fe9dde39269bc1c73
PecanPie <hidra0321@naver.com>
2023-01-30 18:45:22 +0900

    :thread: Added RealmManager File

--- Collageuner/Collageuner/Realm/RealmManager.swift
+++ Collageuner/Collageuner/Realm/RealmManager.swift
@@ -1,5 +1,5 @@
 //
-//  Realm.swift
+//  RealmManager.swift
 //  Collageuner
 //
 //  Created by KYUBO A. SHIM on 2023/01/29.
@@ -11,4 +11,21 @@ import RealmSwift
 import RxCocoa
 import RxSwift
 
+extension Realm {
+    
+    // 일단 이렇게 해놓고 고도화 시켜보자 일단은! write 에 대해서도 정확한 func 을 만들고 해당 thread Safe 나 구동 safe 에 대한 공식적인 이야기가 확실해지면 그때 사용하도록 해보자. 실제 사용하면서 에러도 찾아야 하니깐!
+    static func safeInit() -> Realm? {
+        do {
+            let realm = try Realm()
+            return realm
+        } catch let error {
+            print(error)
+            print("⚠️ Fetching Realm Failed")
+            
+            return nil
+        }
+    }
+    
+    // print("Realm is located at:", realm.configuration.fileURL!)
+}
 
