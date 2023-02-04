24763954e5f70d84f2261468cb0358376ea9a66a
PecanPie <hidra0321@naver.com>
2023-02-04 17:03:13 +0900

    Changed little bit

--- Collageuner/Collageuner/ViewModels/imageAssetViewModel.swift
+++ Collageuner/Collageuner/ViewModels/imageAssetViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  GarageImagesViewModel.swift
+//  imageAssetViewModel.swift
 //  Collageuner
 //
 //  Created by KYUBO A. SHIM on 2023/01/27.
@@ -10,7 +10,21 @@ import UIKit
 import RealmSwift
 import RxCocoa
 import RxSwift
+//
+//class imageAssetViewModel: Object {
+//    @Persisted(primaryKey: true) var _id: ObjectId
+//
+//    @Persisted var imageURL: String
+//    @Persisted var addedDate: Date
+//
+//    init(imageURL: String, addedDate: Date) {
+//        self.init()
+//
+//        self.imageURL = imageURL
+//        self.addedDate = addedDate
+//    }
+//}
 
-class imageAssetViewModel {
+class garageImagesViewModel {
     
 }
