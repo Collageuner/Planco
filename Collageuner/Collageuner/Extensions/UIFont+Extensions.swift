f5bd0ff45704d632adb3fc896e6c447911abc0a2
PecanPie <hidra0321@naver.com>
2023-02-14 01:15:44 +0900

    :lipstick: Added Apple SD Gothic Neo Font to UIExtentsion

--- Collageuner/Collageuner/Extensions/UIFont+Extensions.swift
+++ Collageuner/Collageuner/Extensions/UIFont+Extensions.swift
@@ -23,6 +23,9 @@ extension UIFont {
         return scaledFont
     }
     
+    static func customVersatileFont(_ font: AppleSDNeo, forTextStyle style: UIFont.TextStyle, overrideFontSize: UIContentSizeCategory? = nil) -> UIFont? {
+        return nil
+    }
 }
 
 enum EBGaramond {
@@ -59,6 +62,47 @@ enum EBGaramond {
             }
         }
 }
+
+enum AppleSDNeo {
+    case ultraLight
+    case thin
+    case light
+    case regualar
+    case medium
+    case semibold
+    case bold
+    
+    func fontName() -> String {
+        switch self {
+        case .ultraLight: return "AppleSDGothicNeo-UltraLight"
+        case .thin: return "AppleSDGothicNeo-Thin"
+        case .light: return "AppleSDGothicNeo-Light"
+        case .regualar: return "AppleSDGothicNeo-Regular"
+        case .medium: return "AppleSDGothicNeo-Medium"
+        case .semibold: return "AppleSDGothicNeo-SemiBold"
+        case .bold: return "AppleSDGothicNeo-Bold"
+        }
+    }
+    
+    func fontSize(style: UIFont.TextStyle) -> CGFloat {
+            switch style {
+            case .largeTitle: return 34.0
+            case .title1: return 28.0
+            case .title2: return 22.0
+            case .title3: return 20.0
+            case .headline: return 18.0
+            case .body: return 17.0
+            case .callout: return 16.0
+            case .subheadline: return 15.0
+            case .footnote: return 13.0
+            case .caption1: return 12.0
+            case .caption2: return 11.0
+            default: return 17.0
+            }
+        }
+}
+
+
 
 /*
  EBGaramond-Regular
---
+++ Collageuner/Collageuner/Views/Plan/PlanItemCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PlanItemCollectionViewCell.swift
+//  Collageuner
+//
+//  Created by KYUBO A. SHIM on 2023/02/14.
+//
+
+import UIKit
+
+class PlanItemCollectionViewCell: UICollectionViewCell {
+    
+}
