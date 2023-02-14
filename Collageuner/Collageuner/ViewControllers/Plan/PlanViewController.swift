4413c9330b1eddc323abe04cbf83bacb4fcfa58b
PecanPie <hidra0321@naver.com>
2023-02-14 17:45:01 +0900

    :lipstick: Added GuideUIView to PlanVC

--- Collageuner/Collageuner/ViewControllers/Plan/PlanViewController.swift
+++ Collageuner/Collageuner/ViewControllers/Plan/PlanViewController.swift
@@ -7,11 +7,53 @@
 
 import UIKit
 
-final class PlanViewController: UIViewController {
+import FSCalendar
+import RealmSwift
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
 
+final class PlanViewController: UIViewController {
+    
+    private lazy var weeklyCalendarView = FSCalendar(frame: .zero).then {
+        
+        $0.appearance.selectionColor = .MainCalendar
+        $0.appearance.todayColor = .MainCalendar.withAlphaComponent(0.5)
+        $0.appearance.titleWeekendColor = .SubText
+        $0.appearance.weekdayTextColor = .SubGray.withAlphaComponent(0.8)
+        $0.appearance.titleFont = .customVersatileFont(.medium, forTextStyle: .subheadline)
+        $0.appearance.weekdayFont = UIFont.systemFont(ofSize: 11, weight: .medium)
+        $0.locale = Locale(identifier: "en_US")
+        $0.collectionViewLayout.sectionInsets = UIEdgeInsets(top: 0, left: 0, bottom: 5, right: 0)
+        $0.weekdayHeight = CGFloat(20)
+        $0.calendarHeaderView.isHidden = true
+        $0.headerHeight = 0
+        $0.scrollDirection = .horizontal
+        $0.scrollEnabled = true
+        $0.scope = .week
+    }
+    
+    private let lineDivider = UIView().then {
+        $0.backgroundColor = .MainGray
+    }
+    
+    private let guideBox = PlanFirstGuideView().then {
+        $0.isHidden = false
+    }
+    
+    private let backgroundPlantImage = UIImageView().then {
+        $0.clipsToBounds = true
+        $0.image = UIImage(named: "BackgroundPlantOne")
+        $0.contentMode = .scaleAspectFit
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .Background
+        basicSetup()
+        layouts()
+        bindings()
+        actions()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -21,6 +63,48 @@ final class PlanViewController: UIViewController {
         navigationItem.rightBarButtonItems = [UIBarButtonItem(image: UIImage(systemName: "photo.stack", withConfiguration: symbolConfiguration), style: .plain, target: self, action: #selector(actionThree)), UIBarButtonItem(image: UIImage(systemName: "calendar", withConfiguration: symbolConfiguration),  style: .plain, target: self, action: #selector(actionTwo)), UIBarButtonItem(image: UIImage(systemName: "person.crop.circle.badge.clock", withConfiguration: symbolConfiguration), style: .plain, target: self, action: #selector(actionOne))]
     }
 
+    private func basicSetup() {
+        view.backgroundColor = .Background
+        weeklyCalendarView.delegate = self
+        weeklyCalendarView.dataSource = self
+    }
+    
+    private func layouts() {
+        view.addSubviews(backgroundPlantImage, weeklyCalendarView, lineDivider, guideBox)
+        
+        backgroundPlantImage.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().offset(50)
+            $0.width.equalToSuperview().dividedBy(2.4)
+        }
+        
+        weeklyCalendarView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(10)
+            $0.leading.trailing.equalToSuperview().inset(25)
+            $0.height.equalTo(250)
+        }
+        
+        lineDivider.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(25)
+            $0.top.equalTo(weeklyCalendarView.snp.bottom)
+            $0.height.equalTo(1)
+        }
+        
+        guideBox.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(25)
+            $0.top.equalTo(lineDivider.snp.bottom).offset(15)
+            $0.height.equalToSuperview().dividedBy(2.9)
+        }
+    }
+    
+    private func bindings() {
+    }
+    
+    private func actions() {
+    }
+    
+    
+    
     @objc
     private func actionOne() {
         print("One")
@@ -40,3 +124,25 @@ final class PlanViewController: UIViewController {
         print("Blue Out")
     }
 }
+
+extension PlanViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+    
+    
+    
+    internal func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendar.snp.updateConstraints {
+            $0.height.equalTo(bounds.height)
+        }
+        self.view.layoutIfNeeded()
+    }
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct PlanViewController_PreViews: PreviewProvider {
+    static var previews: some View {
+        PlanViewController().toPreview()
+    }
+}
+#endif
--- Collageuner/Collageuner/Views/Plan/PlanFirstGuideView.swift
+++ Collageuner/Collageuner/Views/Plan/PlanFirstGuideView.swift
@@ -5,4 +5,146 @@
 //  Created by KYUBO A. SHIM on 2023/02/14.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class PlanFirstGuideView: UIView {
+    private let myTimeZoneGuideLabel = UILabel().then {
+        $0.setTextWithLineHeight(text: """
+        당신만의 하루를 풀어내어, 정원을 꾸며주세요!
+        당신의 오전, 이른 오후, 그리고 늦은 오후는 언제인가요?
+        """, lineHeight: 20)
+        $0.numberOfLines = 0
+        $0.textColor = .SubText
+        $0.font = .customVersatileFont(.regualar, forTextStyle: .footnote)
+    }
+    
+    private let myTimeButtonLabel = UILabel().then {
+        $0.textColor = .MainGreen
+        $0.font = .customVersatileFont(.bold, forTextStyle: .callout)
+        $0.text = "나만의 시간 정하기"
+    }
+    
+    private let timeZoneTappedButton = UIImageView().then {
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title3)
+        $0.image = UIImage(systemName: "chevron.right.circle.fill", withConfiguration: symbolConfiguration)
+        $0.clipsToBounds = true
+        $0.tintColor = .MainGreen
+    }
+    
+    private let timeZoneIconButton = UIImageView().then {
+        let symbolConfiguration = UIImage.SymbolConfiguration(pointSize: 20, weight: .semibold)
+        $0.image = UIImage(systemName: "person.crop.circle.badge.clock", withConfiguration: symbolConfiguration)
+        $0.clipsToBounds = true
+        $0.tintColor = .PopGreen
+    }
+    
+    private let myGarageImagesGuideLabel = UILabel().then {
+        $0.setTextWithLineHeight(text: """
+        당신의 정원에는 생김새가 같은 울타리, 잡초, 그리고
+        지저귀는 작은 새들이 있을 수 있죠!
+        """, lineHeight: 20)
+        $0.numberOfLines = 0
+        $0.textColor = .SubText
+        $0.font = .customVersatileFont(.regualar, forTextStyle: .footnote)
+    }
+    
+    private let myGarageButtonLabel = UILabel().then {
+        $0.textColor = .MainGreen
+        $0.font = .customVersatileFont(.bold, forTextStyle: .callout)
+        $0.text = "자주 쓰는 사진 저장하기"
+    }
+    
+    private let myGarageTappedButton = UIImageView().then {
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title3)
+        $0.image = UIImage(systemName: "chevron.right.circle.fill", withConfiguration: symbolConfiguration)
+        $0.clipsToBounds = true
+        $0.tintColor = .MainGreen
+    }
+    
+    private let myGarageIconButton = UIImageView().then {
+        let symbolConfiguration = UIImage.SymbolConfiguration(pointSize: 18, weight: .semibold)
+        $0.image = UIImage(systemName: "photo.stack", withConfiguration: symbolConfiguration)
+        $0.clipsToBounds = true
+        $0.tintColor = .PopGreen
+    }
+    
+    let myTimeZoneStack = UIStackView().then {
+        $0.alignment = .center
+        $0.spacing = 5
+        $0.axis = .horizontal
+    }
+    
+    let myGarageStack = UIStackView().then {
+        $0.alignment = .center
+        $0.spacing = 5
+        $0.axis = .horizontal
+    }
+    
+    let checkedGuideButton = UIButton(type: .system).then {
+        $0.titleLabel?.font = .customVersatileFont(.semibold, forTextStyle: .body)
+        $0.layer.cornerRadius = 15
+        $0.tintColor = UIColor(hex: "#F58C40")
+        $0.backgroundColor = UIColor(hex: "#F8DDAE")
+        $0.setTitle("확인했어요!", for: .normal)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor(hex: "#FAF1E3")
+        updateConstraints()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 20
+    }
+    
+    override func updateConstraints() {
+        render()
+        super.updateConstraints()
+    }
+    
+    private func render() {
+        myTimeZoneStack.addArrangedSubview(myTimeButtonLabel)
+        myTimeZoneStack.addArrangedSubview(timeZoneTappedButton)
+        myTimeZoneStack.addArrangedSubview(timeZoneIconButton)
+        myGarageStack.addArrangedSubview(myGarageButtonLabel)
+        myGarageStack.addArrangedSubview(myGarageTappedButton)
+        myGarageStack.addArrangedSubview(myGarageIconButton)
+        
+        self.addSubviews(myTimeZoneGuideLabel, myTimeZoneStack, myGarageImagesGuideLabel, myGarageStack, checkedGuideButton)
+        
+        myTimeZoneGuideLabel.snp.makeConstraints {
+            $0.leading.top.equalToSuperview().inset(20)
+        }
+        
+        myTimeZoneStack.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.top.equalTo(myTimeZoneGuideLabel.snp.bottom).offset(5)
+        }
+        
+        myGarageImagesGuideLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.top.equalTo(myTimeZoneStack.snp.bottom).offset(14)
+        }
+        
+        myGarageStack.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.top.equalTo(myGarageImagesGuideLabel.snp.bottom).offset(5)
+        }
+        
+        checkedGuideButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(15)
+            $0.height.equalToSuperview().dividedBy(5)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
