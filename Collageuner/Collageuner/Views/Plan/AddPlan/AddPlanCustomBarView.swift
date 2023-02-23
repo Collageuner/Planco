9527c4587ff13d781f506299d88dd09d35ab705d
PecanPie <hidra0321@naver.com>
2023-02-24 00:24:15 +0900

    :lipstick: Added a custom navigation View for AddPlanView

--- Collageuner/Collageuner/Views/Plan/AddPlan/AddPlanCustomBarView.swift
+++ Collageuner/Collageuner/Views/Plan/AddPlan/AddPlanCustomBarView.swift
@@ -7,14 +7,47 @@
 
 import UIKit
 
-class AddPlanCustomBarView: UIView {
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class AddPlanCustomBarView: UIView {
+    
+    let cancelButton = UIButton(type: .system).then {
+        $0.setAttributedTitle(NSAttributedString(string: "Cancel", attributes: [NSAttributedString.Key.font : UIFont.customEnglishFont(.medium, forTextStyle: .title3) ?? UIFont.preferredFont(forTextStyle: .title3, weight: .medium)]), for: .normal)
+        $0.tintColor = .gray
     }
-    */
+    
+    let doneButton = UIButton(type: .system).then {
+        $0.setAttributedTitle(NSAttributedString(string: "Done", attributes: [NSAttributedString.Key.font : UIFont.customEnglishFont(.bold, forTextStyle: .title3) ?? UIFont.preferredFont(forTextStyle: .title3, weight: .medium)]), for: .normal)
+        $0.tintColor = .MainGreen
+    }
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .clear
+        updateConstraints()
+    }
+    
+    override func updateConstraints() {
+        render()
+        super.updateConstraints()
+    }
+    
+    private func render() {
+        self.addSubviews(cancelButton, doneButton)
+        
+        cancelButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(15)
+            $0.centerY.equalToSuperview()
+        }
+        
+        doneButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(15)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
