00e12213e8eae172b41f8633672e77d9e7258656
PecanPie <hidra0321@naver.com>
2023-02-27 16:58:52 +0900

    :lipstick: Modified GarageSheet VC and ViewCell
    Used Delegate to pass garage Image which is picked

--- Collageuner/Collageuner/Views/Plan/AddPlan/GarageSheetCollectionViewCell.swift
+++ Collageuner/Collageuner/Views/Plan/AddPlan/GarageSheetCollectionViewCell.swift
@@ -9,4 +9,41 @@ import UIKit
 
 class GarageSheetCollectionViewCell: UICollectionViewCell {
     
+    let garageImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.layer.masksToBounds = true
+        $0.backgroundColor = .clear
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        updateConstraints()
+    }
+    
+    override func updateConstraints() {
+        render()
+        super.updateConstraints()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        garageImageView.clipsToBounds = true
+        garageImageView.layer.cornerRadius = 15
+    }
+    
+    private func render() {
+        self.addSubview(garageImageView)
+        
+        garageImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        print("GarageSheetOut")
+    }
 }
