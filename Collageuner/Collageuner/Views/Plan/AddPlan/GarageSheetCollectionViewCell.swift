1828ec840547b7d6f2b81549cf3d06543bdd54ff
PecanPie <hidra0321@naver.com>
2023-02-27 16:51:35 +0900

    :lipstick: Added GarageImage Bottom VC and ViewCell

---
+++ Collageuner/Collageuner/Views/Plan/AddPlan/GarageSheetCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GarageSheetCollectionViewCell.swift
+//  Collageuner
+//
+//  Created by KYUBO A. SHIM on 2023/02/27.
+//
+
+import UIKit
+
+class GarageSheetCollectionViewCell: UICollectionViewCell {
+    
+}
