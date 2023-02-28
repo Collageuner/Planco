75b781f9c11aa8c29e0de2d3137045d7b5610564
PecanPie <hidra0321@naver.com>
2023-02-28 17:02:40 +0900

    :lipstick: Added LoadingView UIView

--- Collageuner/Collageuner/Views/Plan/AddPlan/AwaitLoadingView.swift
+++ Collageuner/Collageuner/Views/Plan/AddPlan/AwaitLoadingView.swift
@@ -5,4 +5,40 @@
 //  Created by KYUBO A. SHIM on 2023/02/27.
 //
 
-import Foundation
+import UIKit
+
+import Lottie
+
+final class AwaitLoadingView: UIView {
+    
+    private let loadingImage: LottieAnimationView = .init(name: "loadingView").then {
+        $0.loopMode = .loop
+        $0.animationSpeed = 1.3
+        $0.contentMode = .scaleAspectFit
+        $0.play(toFrame: 300)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        updateConstraints()
+    }
+    
+    override func updateConstraints() {
+        render()
+        super.updateConstraints()
+    }
+    
+    private func render() {
+        self.addSubview(loadingImage)
+        
+        loadingImage.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.width.equalTo(300)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
