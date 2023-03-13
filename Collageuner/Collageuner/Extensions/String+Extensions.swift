e97a3de9269be48d4bebdd2486b8317b7b6e023d
PecanPie <hidra0321@naver.com>
2023-03-13 20:04:50 +0900

    Added calculated Properties to change Date to String as h:ss:am

--- Collageuner/Collageuner/Extensions/String+Extensions.swift
+++ Collageuner/Collageuner/Extensions/String+Extensions.swift
@@ -15,4 +15,38 @@ extension String {
             return self
         }
     }
+    
+    /// Returns h:mm am
+    var changeAmPmToString: String {
+        let hour: String = String(self.prefix(2))
+        let minute: String = String(self.suffix(2))
+        let hourToInt: Int = Int(hour) ?? 0
+        
+        switch hourToInt {
+        case 0...11:
+            return "\(hourToInt):\(minute) am"
+        case 12...23:
+            return "\(hourToInt - 12):\(minute) pm"
+        default:
+            print("Error changing time h:mm AmPm")
+            return ""
+        }
+    }
+    
+    /// Returns h:mm
+    var changeHourIntoShort: String {
+        let hour: String = String(self.prefix(2))
+        let minute: String = String(self.suffix(2))
+        let hourToInt: Int = Int(hour) ?? 0
+        
+        switch hourToInt {
+        case 0...11:
+            return "\(hourToInt):\(minute)"
+        case 12...23:
+            return "\(hourToInt - 12):\(minute)"
+        default:
+            print("Error changing time h:mm")
+            return ""
+        }
+    }
 }
