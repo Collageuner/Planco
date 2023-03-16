ee98db0e5668ea53db3c1f7881fc1b89c7e97402
PecanPie <hidra0321@naver.com>
2023-03-16 16:51:50 +0900

    :sparkles: Added a Custom GalleryView for Selecting a Image
    1. Added GalleryCollectionViewCell 2.Used Photos Framework instead of PhotosUI (PHPickerVC)

--- Collageuner/Collageuner/ViewControllers/Garage/GalleryViewController.swift
+++ Collageuner/Collageuner/ViewControllers/Garage/GalleryViewController.swift
@@ -15,36 +15,111 @@ import SnapKit
 import Then
 
 final class GalleryViewController: UIViewController {
+    var disposeBag = DisposeBag()
+    
+    private var asset: PHFetchResult<PHAsset>!
+
+    private let phFetchOptions = PHFetchOptions().then {
+        $0.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+    }
+    
+    let imageManager = PHCachingImageManager().then {
+        $0.allowsCachingHighQualityImages = true
+    }
+    
+    let customNavigationView = ModalCustomBarView()
+    
     private lazy var galleryCollectionView = UICollectionView(frame: .zero, collectionViewLayout: galleryCollectionViewLayout).then {
         $0.scrollsToTop = true
-        $0.register(GardenListCollectionViewCell.self, forCellWithReuseIdentifier: IdsForCollectionView.GardenListCollectionViewId.identifier)
-        $0.backgroundColor = .red
+        $0.register(GalleryCollectionViewCell.self, forCellWithReuseIdentifier: IdsForCollectionView.GalleryCollectionItemId.identifier)
+        $0.backgroundColor = .clear
     }
     
     private lazy var galleryCollectionViewLayout = UICollectionViewFlowLayout().then {
-        let cellSize = (view.frame.width - 20)/3
+        let cellSize = (view.frame.width - 30)/3
         $0.itemSize = CGSize(width: cellSize, height: cellSize)
-        $0.minimumLineSpacing = 10
-        $0.minimumInteritemSpacing = 10
+        $0.minimumLineSpacing = 5
+        $0.minimumInteritemSpacing = 5
         $0.scrollDirection = .vertical
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         basicSetup()
         layouts()
         bindings()
+        actions()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setToolbarHidden(true, animated: true)
     }
     
     private func basicSetup() {
+        view.backgroundColor = .black
+        fetchAssetInitialize()
         
+        galleryCollectionView.delegate = self
+        galleryCollectionView.dataSource = self
     }
     
     private func layouts() {
+        view.addSubviews(customNavigationView, galleryCollectionView)
         
+        customNavigationView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalToSuperview().dividedBy(15.5)
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        galleryCollectionView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(10)
+            $0.bottom.equalToSuperview()
+            $0.top.equalTo(customNavigationView.snp.bottom).offset(15)
+        }
     }
     
     private func bindings() {
         
+    }
+    
+    private func actions() {
+        let cancelTapped = UITapGestureRecognizer(target: self, action: #selector(dismissView))
+        customNavigationView.cancelButton.addGestureRecognizer(cancelTapped)
+    }
+    
+    private func fetchAssetInitialize() {
+        asset = PHAsset.fetchAssets(with: phFetchOptions)
+    }
+    
+    @objc
+    private func dismissView() {
+        self.dismiss(animated: true)
+    }
+    
+    deinit {
+        print("GalleryView Out")
+    }
+}
+
+extension GalleryViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return asset.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IdsForCollectionView.GalleryCollectionItemId.identifier, for: indexPath) as? GalleryCollectionViewCell else { return UICollectionViewCell()}
+        let thumbnailSize = 270
+        let asset = self.asset[indexPath.item]
+        cell.representedAssetIdentifier = asset.localIdentifier
+
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: thumbnailSize, height: thumbnailSize), contentMode: .aspectFit, options: nil) { image, _ in
+            if cell.representedAssetIdentifier == asset.localIdentifier {
+                cell.putImageToImageView(image)
+            }
+        }
+        
+        return cell
     }
 }
--- Collageuner/Collageuner/Views/Garage/GalleryCollectionViewCell.swift
+++ Collageuner/Collageuner/Views/Garage/GalleryCollectionViewCell.swift
@@ -7,6 +7,41 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class GalleryCollectionViewCell: UICollectionViewCell {
+    var representedAssetIdentifier: String?
     
+    let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+        $0.backgroundColor = .clear
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        updateConstraints()
+    }
+    
+    override func updateConstraints() {
+        render()
+        super.updateConstraints()
+    }
+    
+    private func render() {
+        self.addSubview(imageView)
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func putImageToImageView(_ image: UIImage?) {
+        imageView.image = image
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
