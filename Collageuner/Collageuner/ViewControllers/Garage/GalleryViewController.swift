e046391f034baa0a734c303b380e0d7de2fa12ee
PecanPie <hidra0321@naver.com>
2023-03-20 21:44:47 +0900

    :art: Seperated the func that fetches data as a cell being selected and made it async await to get highest quality image

--- Collageuner/Collageuner/ViewControllers/Garage/GalleryViewController.swift
+++ Collageuner/Collageuner/ViewControllers/Garage/GalleryViewController.swift
@@ -15,19 +15,20 @@ import SnapKit
 import Then
 
 final class GalleryViewController: UIViewController {
-    var disposeBag = DisposeBag()
     private let garageViewModel = GarageImagesViewModel()
+    private lazy var garageImages: [GarageImage] = garageViewModel.garageImages.value
     
     weak var delegate: GarageViewDelegate?
 
     private var asset: PHFetchResult<PHAsset>!
+    private var selectedAsset: PHAsset! = nil
     private var selectedPngData: Data? = nil
 
     private let phFetchOptions = PHFetchOptions().then {
         $0.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
     }
     
-    private let imageManager = PHCachingImageManager().then {
+    private let imageCachingManager = PHCachingImageManager().then {
         $0.allowsCachingHighQualityImages = true
     }
     
@@ -51,7 +52,6 @@ final class GalleryViewController: UIViewController {
         super.viewDidLoad()
         basicSetup()
         layouts()
-        bindings()
         actions()
     }
     
@@ -63,7 +63,7 @@ final class GalleryViewController: UIViewController {
     private func basicSetup() {
         view.backgroundColor = .black
 
-        fetchAssetInitialize()
+        initializePHAsset()
         PHPhotoLibrary.shared().register(self)
         galleryCollectionView.delegate = self
         galleryCollectionView.dataSource = self
@@ -85,10 +85,6 @@ final class GalleryViewController: UIViewController {
         }
     }
     
-    private func bindings() {
-        
-    }
-    
     private func actions() {
         let nextTapped = UITapGestureRecognizer(target: self, action: #selector(moveToConfirm))
         let cancelTapped = UITapGestureRecognizer(target: self, action: #selector(dismissView))
@@ -96,22 +92,49 @@ final class GalleryViewController: UIViewController {
         customNavigationView.doneButton.addGestureRecognizer(nextTapped)
         customNavigationView.cancelButton.addGestureRecognizer(cancelTapped)
     }
+        
+    private func saveAssetBeforeTap(_ asset: PHAsset) async {
+        let imageManager = PHImageManager()
+        let imageOptions = PHImageRequestOptions()
+        imageOptions.isSynchronous = false
+        imageOptions.deliveryMode = .highQualityFormat
+        imageOptions.isNetworkAccessAllowed = true
+        
+        imageManager.requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: imageOptions) { [weak self] image, _ in
+            self?.selectedPngData = image?.pngData()
+        }
+        
+        
+    }
     
-    private func fetchAssetInitialize() {
-        asset = PHAsset.fetchAssets(with: phFetchOptions)
+    private func passThumbnailImageToGarageViewController(_ asset: PHAsset) {
+        let imageManager = PHImageManager()
+
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, _ in
+            // reloadData Delegate
+        }
+    }
+    
+    private func initializePHAsset() {
+        asset = PHAsset.fetchAssets(with: .image, options: phFetchOptions)
     }
     
     @objc
     private func moveToConfirm() {
-        if selectedPngData != nil {
-            guard let pngData = selectedPngData else {
-                print("Changing selected PHAsset to png Data Failed")
-                return
+        if selectedAsset != nil {
+            Task {
+                await saveAssetBeforeTap(selectedAsset)
+                
+                guard let pngData = selectedPngData else {
+                    print("Changing selected PHAsset to png Data Failed")
+                    return
+                }
+                guard let pngImage = UIImage(data: pngData) else { return }
+                
+                garageViewModel.addGarageImage(garageImage: pngImage)
+                self.delegate?.reloadTableViews()
+                self.dismiss(animated: true)
             }
-            guard let pngImage = UIImage(data: pngData) else { return }
-            garageViewModel.addGarageImage(garageImage: pngImage)
-            self.delegate?.reloadTableViews()
-            self.dismiss(animated: true)
         } else {
             print("Nil has been selected")
             self.dismiss(animated: true)
@@ -135,12 +158,12 @@ extension GalleryViewController: UICollectionViewDelegate, UICollectionViewDataS
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let thumbnailSize = 300
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IdsForCollectionView.GalleryCollectionItemId.identifier, for: indexPath) as? GalleryCollectionViewCell else { return UICollectionViewCell()}
-        let thumbnailSize = 270
         let asset = self.asset[indexPath.item]
         cell.representedAssetIdentifier = asset.localIdentifier
-
-        imageManager.requestImage(for: asset, targetSize: CGSize(width: thumbnailSize, height: thumbnailSize), contentMode: .aspectFit, options: nil) { image, _ in
+        
+        imageCachingManager.requestImage(for: asset, targetSize: CGSize(width: thumbnailSize, height: thumbnailSize), contentMode: .aspectFit, options: nil) { image, _ in
             if cell.representedAssetIdentifier == asset.localIdentifier {
                 cell.putImageToImageView(image)
             }
@@ -150,18 +173,9 @@ extension GalleryViewController: UICollectionViewDelegate, UICollectionViewDataS
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(indexPath.item)
-        let imageManager = PHImageManager()
-        let imageOptions = PHImageRequestOptions()
-        imageOptions.isNetworkAccessAllowed = true
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IdsForCollectionView.GalleryCollectionItemId.identifier, for: indexPath) as? GalleryCollectionViewCell else { return }
         let asset = self.asset[indexPath.item]
-        cell.representedAssetIdentifier = asset.localIdentifier
         
-        
-        imageManager.requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: imageOptions) { [weak self] image, _ in
-            self?.selectedPngData = image?.pngData()
-        }
+        selectedAsset = asset
     }
 }
 
