78855afa2916fe6f5925357846e9e126c4c57260
PecanPie <hidra0321@naver.com>
2023-03-25 00:49:55 +0900

    Added NavigationBar to it and Modified it

--- Collageuner/Collageuner/ViewControllers/Plan/AddPlan/TaskCompleteImagePickerViewController.swift
+++ Collageuner/Collageuner/ViewControllers/Plan/AddPlan/TaskCompleteImagePickerViewController.swift
@@ -16,9 +16,36 @@ import Then
 
 final class TaskCompleteImagePickerViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .Background
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItemSetup()
     }
 
+    private func navigationItemSetup() {
+        navigationController?.navigationBar.tintColor = .MainGreen
+        navigationItem.title = "기록하기"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(moveNext))
+    }
+    
+    @objc
+    private func moveNext() {
+        
+    }
+    
 }
+
+//#if canImport(SwiftUI) && DEBUG
+//import SwiftUI
+//
+//struct PlanViewController_PreViews: PreviewProvider {
+//    static var previews: some View {
+//        TaskCompleteImagePickerViewController().toPreview()
+//    }
+//}
+//#endif
