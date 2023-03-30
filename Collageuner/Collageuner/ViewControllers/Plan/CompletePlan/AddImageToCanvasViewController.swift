52d272eb040b1e67901f9186aab5dbb811ecbeec
PecanPie <hidra0321@naver.com>
2023-03-30 23:06:47 +0900

    Moved and changed some files and folders

---
+++ Collageuner/Collageuner/ViewControllers/Plan/CompletePlan/AddImageToCanvasViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddImageToCanvasViewController.swift
+//  Collageuner
+//
+//  Created by KYUBO A. SHIM on 2023/03/27.
+//
+
+import UIKit
+
+class AddImageToCanvasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ Collageuner/Collageuner/Views/Common/StickerView.swift
@@ -0,0 +1,20 @@
+//
+//  StickerView.swift
+//  Collageuner
+//
+//  Created by KYUBO A. SHIM on 2023/03/30.
+//
+
+import UIKit
+
+class StickerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
